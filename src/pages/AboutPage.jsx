85ff6196ac3a17455d096566af6ca99a2a48c892
PotatoDoge1 <ryan.a.schwark@gmail.com>
2024-11-03 20:19:40 -0500

    added form functionality with limited data validation checks. also added some additional styling.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,11 +2,11 @@ import profilePic from '../assets/doge.jpeg';
 
 export default function AboutPage() {
     return (
-        <div className="text-center">
-            <h1>I am PotatoDoge1</h1>
+        <div>
+            <h1  className="text-center">About Me</h1>
             <img src={profilePic} alt="PotatoDoge1" className="img-fluid"></img>
-            <h2>Bio:</h2>
-            <p>I am learning to code part-time while working as a financial analyst.</p>
+            <h2  className="text-center">I am PotatoDoge1</h2>
+            <p  className="text-center">I am learning to code part-time while working as a financial analyst. The facebook link below does not work. That is intentional.</p>
         </div>
     );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
+import ContactForm from '../Components/Forms/ContactForm.jsx';
+
 export default function Contact() {
     return(
         <div>
             <h1>Contact Page</h1>
-            <p> I am PotatoDoge1 on Github </p>
+            <p> I am PotatoDoge1 on Github. In a bit, you will be able to send me a message through the form below! </p>
+            <ContactForm />
         </div>
     )
 }
